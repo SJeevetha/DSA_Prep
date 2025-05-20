fca8d026d3057c35fed2e8465f064f2207982553
SJeevetha <126893867+SJeevetha@users.noreply.github.com>
2025-05-20 20:39:54 +0530

    Create Pattern12.java

---
+++ week2/Pattern12.java
@@ -0,0 +1,34 @@
+/* if n = 4
+Expected input:
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *        */
+import java.util.Scanner;
+class Pattern12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int s=1;s<=n-i;s++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
