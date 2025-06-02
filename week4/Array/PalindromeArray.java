5e9c0031bfdcac0cc7b1de96b6fa12698b921321
SJeevetha <126893867+SJeevetha@users.noreply.github.com>
2025-06-03 01:20:55 +0530

    Create PalindromeArray.java

---
+++ week4/Array/PalindromeArray.java
@@ -0,0 +1,17 @@
+// https://www.geeksforgeeks.org/problems/perfect-arrays4645/1
+
+class Solution {
+    public static boolean isPerfect(int[] arr) {
+        // code here
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            if(arr[start]!=arr[end]){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
