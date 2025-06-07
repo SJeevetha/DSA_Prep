ed3fe5f535efbc80f7a180ca94556af62efc0c29
SJeevetha <126893867+SJeevetha@users.noreply.github.com>
2025-06-07 22:39:48 +0530

    Create fibonacci.java

---
+++ week4/Array_Recursion/fibonacci.java
@@ -0,0 +1,12 @@
+//  https://www.geeksforgeeks.org/problems/nth-fibonacci-number1335/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+class Solution {
+    public int nthFibonacci(int n) {
+        // code here
+        if(n==1)
+         return 1;
+        else if(n==0)
+         return 0;
+        return nthFibonacci(n-1)+nthFibonacci(n-2); 
+    }
+}
